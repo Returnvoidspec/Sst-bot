905fdde47514d917a321ffa477b5466a8167854c
Returnvoidspec <martin.jarnier@epita.fr>
2024-02-28 10:39:50 +0100

    lol

--- sst-bot/sst_bot/main.py
+++ sst-bot/sst_bot/main.py
@@ -7,13 +7,42 @@ def analysis():
     bot_to_path()"""
     path = None
     diarization_mod = Diarization()
-    sst_text = diarization_mod.Audio_to_text(path)
+    # sst_text = diarization_mod.Audio_to_text(path)
+    sst_text = """SPEAKER_01 :  Hello.
+SPEAKER_00 :  Hi.
+SPEAKER_01 :  What can I get you? Coffee? Tea?
+SPEAKER_00 :  Tea, please.
+SPEAKER_01 :  Milk and sugar?
+SPEAKER_00 :  No milk, please. But sugar.
+SPEAKER_01 :  Would you like anything to eat?
+SPEAKER_01 :  A slice of chocolate cake?
+SPEAKER_01 :  Some pastries?
+SPEAKER_00 :  Tempting, but no thanks. Just tea.
+SPEAKER_01 :  Amazing!
+SPEAKER_00 :  Who? Oh, I mean what?
+SPEAKER_01 :  Egypt! What an incredible place.
+SPEAKER_00 :  Yes, it's fascinating.
+SPEAKER_00 :  Are you interested in traveling?
+SPEAKER_01 :  Oh, yes. I love traveling.
+SPEAKER_01 :  I haven't got much time.
+SPEAKER_01 :  I'm with the bar and everything.
+SPEAKER_01 :  But there are so many places I'd like to visit.
+SPEAKER_01 :  Do you travel a lot?
+SPEAKER_00 :  Yes, I do.
+SPEAKER_01 :  Lucky you.
+SPEAKER_01 :  Have you got a lot of free time?
+SPEAKER_00 :  Not really. I travel for my job.
+SPEAKER_01 :  Oh, wonderful!
+SPEAKER_01 :  What do you do?
+SPEAKER_00 :  I'm a photographer."""
 
     summarization_mod = TextSummarizer(sst_text)
     summary = summarization_mod.get_summary()
 
     sentimentalize_mod = TextSentiment(sst_text)
-    sentimentalize_mod.get_sentiments()
+    print(sentimentalize_mod.get_sentiments())
+    print(summary)
+    print(sst_text)
 
 
 def main():
--- sst-bot/sst_bot/nlp/infos.py
+++ sst-bot/sst_bot/nlp/infos.py
@@ -1,6 +1,9 @@
 from transformers import BartTokenizer, BartForConditionalGeneration
 import re
 from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
+import spacy
+from collections import Counter
+from string import punctuation
 
 
 class TextSummarizer:
@@ -38,6 +41,41 @@ class TextSummarizer:
     def get_summary(self) -> str:
         return self.summarize_text(self.sst_text)
 
+    def get_relevance_words(self):
+        raw_text = ""
+        nlp = spacy.load("en_core_web_sm")
+        for i in range(self.number_people):
+            pattern = re.compile(rf"SPEAKER_{'0' if i < 10 else ''}{i} :  (.+)")
+            sentences = pattern.findall(self.sst_text)
+            for j in sentences:
+                raw_text += j
+
+        result = set()
+        pos_tag = ['PROPN', 'ADJ', 'NOUN']
+        doc = nlp(raw_text.lower())
+        for token in doc:
+            if token.text in nlp.Defaults.stop_words or token.text in punctuation:
+                continue
+            if token.pos_ in pos_tag:
+                result.add(token.text)
+
+        doc = nlp(self.get_summary().lower())
+        for token in doc:
+            if token.text in nlp.Defaults.stop_words or token.text in punctuation:
+                continue
+            if token.pos_ in pos_tag and token.text not in result:
+                result.add(token.text)
+        return result
+
+
+def sentiments_analysis(speaker_sentences: [str]):
+    analyzer = SentimentIntensityAnalyzer()
+    sentiments = []
+    for position, sentence in speaker_sentences:
+        vs = analyzer.polarity_scores(sentence)
+        sentiments[position] = str(vs)
+    return sentiments
+
 
 class TextSentiment:
     def __init__(self, sst_text: str, number_people: int = 2):
@@ -56,14 +94,13 @@ class TextSentiment:
                 speaker_sentences.append([])
         return speaker_sentences
 
-    def sentiments_analysis(self, speaker_sentences: [str]):
-        analyzer = SentimentIntensityAnalyzer()
-        sentiments = []
-        for position, sentence in speaker_sentences:
-            vs = analyzer.polarity_scores(sentence)
-            sentiments[position] = str(vs)
-        return sentiments
-
     def get_sentiments(self):
         speaker_sentences = self.get_speaker_sentences()
-        return self.sentiments_analysis(speaker_sentences)
+        return sentiments_analysis(speaker_sentences)
+
+    def count_words_per_speaker(self):
+        sentences = self.get_speaker_sentences()
+        speakers = []
+        for i, j in enumerate(sentences):
+            speakers[i] = len(re.findall(r'\w+', j))
+        return speakers
