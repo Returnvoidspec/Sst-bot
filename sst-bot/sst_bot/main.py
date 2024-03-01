a988911bc4c63c4105bcad4d834ad2da57c4840b
Martin <martin.jarnier@epita.fr>
2024-03-01 22:31:50 +0100

    final

--- sst-bot/sst_bot/main.py
+++ sst-bot/sst_bot/main.py
@@ -1,3 +1,5 @@
+import os
+
 from sst.Diarization import Diarization
 from nlp.infos import TextSentiment, TextSummarizer, TextAnalysis
 
@@ -25,8 +27,17 @@ def analysis(path = "test_conv_short.wav"):
 
 
 def main():
-    path = input("Please enter the path to the audio file: ")
-    analysis(path)
+    while True:
+        path = input("Please enter the path to the audio file: ")
+        if os.path.isfile(path):
+            try:
+                analysis(path)
+                break  # Exit the loop if the analysis is successful
+            except Exception as e:
+                print(f"An error occurred: {e}")
+                print("Please try again.")
+        else:
+            print("The path does not point to a valid file. Please reselect a file.")
 
 
 if __name__ == '__main__':
